9e66c86268fb8844ddec0468bdcf4b81cec7c74d
sessoukodjogolden <sessoukodjogolden@gmail.com>
2025-02-17 00:33:16 +0000

    modification de Pharmacies.dart & Produits/dart

--- lib/models/pharmacies.dart
+++ lib/models/pharmacies.dart
@@ -1,34 +1,58 @@
+import 'package:miabe_pharmacie/models/produits.dart';
 class Pharmacie {
-  late final String id;
-  late final String nom;
-  late final String emplacement;
-  late final int telephone1;
-  late final double latitude;
-  late final double longitude;
-  late final String ouverture;
-  late final String fermeture;
+  final String emplacement;
+  final String fermeture;
+  final double latitude;
+  final double longitude;
+  final String nom;
+  final String ouverture;
+  final String telephone1;
+  final String telephone2;
+  final List<Produit> produits;
 
   Pharmacie({
-    required this.id,
-    required this.nom,
     required this.emplacement,
-    required this.telephone1,
-    required this.longitude,
-    required this.latitude,
-    required this.ouverture,
     required this.fermeture,
+    required this.latitude,
+    required this.longitude,
+    required this.nom,
+    required this.ouverture,
+    required this.telephone1,
+    required this.telephone2, 
+    required this.produits,
+
   });
 
-  factory Pharmacie.fromMap(Map<String, dynamic> map) {
+  factory Pharmacie.fromJson(Map<String, dynamic> json) {
+    var produitsList = json['produits'] as List;
+    List<Produit> produits = produitsList.map((i) => Produit.fromJson(i)).toList();
+
     return Pharmacie(
-      id: map['id'],
-      nom: map['nom'],
-      emplacement: map['emplacement'],
-      telephone1: map['telephone1'],
-      latitude: map['latitude'],
-      longitude: map['longitude'],
-      ouverture: map['ouverture'],
-      fermeture: map['fermeture'],
+      emplacement: json['emplacement'],
+      fermeture: json['fermeture'],
+      latitude: json['latitude'],
+      longitude: json['longitude'],
+      nom: json['nom'],
+      ouverture: json['ouverture'],
+      telephone1: json['telephone1'],
+      telephone2: json['telephone2'],
+      produits: produits,
     );
+  }
+
+
+
+
+  Map<String, dynamic> toJson() {
+    return {
+      'emplacement': emplacement,
+      'fermeture': fermeture,
+      'latitude': latitude,
+      'longitude': longitude,
+      'nom': nom,
+      'ouverture': ouverture,
+      'telephone1': telephone1,
+      'telephone2': telephone2,
+    };
   }
 }
